bf394e692f5caf4bc7b9abf178849cf17fd5d8fb
ThuanLeMerkle <Thuan.Le@merkle.com>
2023-12-11 21:00:58 +0700

    show tag filter

--- src/screen/home/component/Search.tsx
+++ src/screen/home/component/Search.tsx
@@ -1,24 +1,29 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
-import { TextInput } from "react-native-gesture-handler";
-import { style } from "../Style";
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { TextInput } from 'react-native-gesture-handler';
+import { style } from '../Style';
 
 export default function Search(){
     const iconSearch = require('../../../assets/icons/search.png');
     const filterContent = [
         {
-            title: "Full-time",
+            title: 'Full-time',
             isCheck: true,
         },
         {
-            title: "Full-time",
-            isCheck: true,
+            title: 'Part-time',
+            isCheck: false,
         },
         {
-            title: "Full-time",
-            isCheck: true,
+            title: 'Contractor',
+            isCheck: false,
         },
-    ]
+    ];
+    const renderFilter = () => filterContent.map((item, index) =>
+                                <TouchableOpacity key={index} style={[style.btnFilter, item.isCheck ? null : style.visible]}>
+                                    <Text style={item.isCheck ? null : style.textVisible}>{item.title}</Text>
+                                </TouchableOpacity>
+                        );
     return (
             <View>
                     {/* Search*/}
@@ -30,17 +35,9 @@ export default function Search(){
                     </View>
                     {/* Filter*/}
                     <View style={style.containerFilter}>
-                        <TouchableOpacity style={style.btnFilter}>
-                            <Text>Full-time</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={[style.btnFilter,style.visible]}>
-                            <Text style={style.textVisible}>Part-time-time</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={[style.btnFilter,style.visible]}>
-                            <Text style={style.textVisible}>Contractor</Text>
-                        </TouchableOpacity>
+                        {renderFilter()}
                     </View>
             </View>
-        
-    )
+
+    );
 }
