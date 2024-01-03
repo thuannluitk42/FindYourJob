f45e5972bfe150b1a00e53f11548d1a0e67ec09a
ThuanLeMerkle <Thuan.Le@merkle.com>
2024-01-03 21:32:05 +0700

    done ui detail page b19

--- src/screen/home/component/Search.tsx
+++ src/screen/home/component/Search.tsx
@@ -1,11 +1,11 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput } from 'react-native-gesture-handler';
 import { style } from '../Style';
 
 export default function Search(){
     const iconSearch = require('../../../assets/icons/search.png');
-    const filterContent = [
+    var contentSearch = [
         {
             title: 'Full-time',
             isCheck: true,
@@ -18,9 +18,21 @@ export default function Search(){
             title: 'Contractor',
             isCheck: false,
         },
-    ];
+    ]
+    const [filterContent, setFilterContent] = useState(contentSearch);
+    const onPressSearchItem = (indexSelected: number) => {
+        const newContentSearch = contentSearch.map((item, index) => {
+            if(index == indexSelected) {
+                item.isCheck = true
+            } else {
+                item.isCheck = false
+            }
+            return item
+        })
+        setFilterContent(newContentSearch)
+    }
     const renderFilter = () => filterContent.map((item, index) =>
-                                <TouchableOpacity key={index} style={[style.btnFilter, item.isCheck ? null : style.visible]}>
+                                <TouchableOpacity key={index} style={[style.btnFilter, item.isCheck ? null : style.visible]} onPress={() => onPressSearchItem(index)}>
                                     <Text style={item.isCheck ? null : style.textVisible}>{item.title}</Text>
                                 </TouchableOpacity>
                         );
